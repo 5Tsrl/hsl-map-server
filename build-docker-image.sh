69ce753fe1dd28c71dbc15e799025a9f52c896fc
aguiraf <aguiraf@gmail.com>
2016-12-06 15:30:23 +0100

    Add DockerHub endpoint

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Set these environment variables
-#DOCKER_TAG=
-#DOCKER_EMAIL=
-#DOCKER_USER=
+DOCKER_TAG=0.1
+DOCKER_EMAIL=aguiraf@gmail.com
+DOCKER_USER=aguiraf
 #DOCKER_AUTH=
 
 # Build image
-SNAP_IMAGE="hsldevcom/hsl-map-server:$DOCKER_TAG"
-LATEST_IMAGE="hsldevcom/hsl-map-server:latest"
+SNAP_IMAGE="5tdev/matp-server:$DOCKER_TAG"
+LATEST_IMAGE="5tdev/matp-server:latest"
 
 docker build --tag=$SNAP_IMAGE .
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
 docker push $SNAP_IMAGE
-docker tag -f $SNAP_IMAGE $LATEST_IMAGE
+docker tag  $SNAP_IMAGE $LATEST_IMAGE
 docker push $LATEST_IMAGE
